d4fbb0453476add14665f5b55f6a2747754f5b31
SUDHANSHU RANJAN <sudhanshuranjan0109@gmail.com>
2026-06-03 08:09:50 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
+}
+
+circleMouseFollower();
